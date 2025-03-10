7aea761f178813b41f6ec429d9da1ea4efd43e12
Saamir <saamirkhan7513@gmail.com>
2025-03-11 00:50:17 +0530

    Added

---
+++ butterfly.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < i + 1; j++)
+        {
+
+            cout << "*";
+        }
+        // space1
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // space2
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        // space1
+        for (int j = i; j > 0; j--)
+        {
+
+            cout << " ";
+        }
+        // space2
+        for (int j = i; j > 0; j--)
+        {
+
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
